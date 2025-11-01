9017761b1c77eecdff0ea708ff7028323c6d1c95
CodingFarmer999 <>
2025-11-01 18:08:11 +0800

    專案結構

---
+++ exp-guessgame-lab/src/main/java/com/course/ExpGuessgameLabApplication.java
@@ -0,0 +1,13 @@
+package com.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpGuessgameLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpGuessgameLabApplication.class, args);
+	}
+
+}
