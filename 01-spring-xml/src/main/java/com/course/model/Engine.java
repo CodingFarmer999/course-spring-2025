d5f31458f314d16b340c18381dca9c70e95d2861
CodingFarmer999 <>
2025-11-10 22:10:05 +0800

    remove

--- 01-spring-xml/src/main/java/com/course/model/Engine.java
+++
@@ -1,6 +0,0 @@
-package com.course.model;
-
-public interface Engine {
-	
-	public void start();
-}
--- 01-spring-xml/src/main/java/com/course/model/ToyotaEngine.java
+++
@@ -1,8 +0,0 @@
-package com.course.model;
-
-public class ToyotaEngine implements Engine {
-
-	public void start() {
-		System.out.println("ToyotaEngine Start...");
-	}
-}
