88bb7d6360e0ce9f702ee96b9119ab914ea7a171
CodingFarmer999 <>
2025-11-14 23:04:10 +0800

    logger

--- 02-springboot-start/src/main/java/com/course/Application.java
+++ 02-springboot-start/src/main/java/com/course/Application.java
@@ -1,5 +1,7 @@
 package com.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,14 +12,23 @@ import com.course.model.Bmw;
 @SpringBootApplication
 public class Application {
 
+	
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		
+		Logger logger = LoggerFactory.getLogger(Application.class);
 		
 		Benz benz = ctx.getBean(Benz.class);
 		benz.move();
 		
 		Bmw bmw = ctx.getBean(Bmw.class);
 		bmw.move();
+		
+		logger.trace("我是 trace");
+		logger.debug("我是 debug");
+		logger.info("我是 info");
+		logger.warn("我是 warn");
+		logger.error("我是 error {}", "凱蒂貓");
 	}
 
 }
