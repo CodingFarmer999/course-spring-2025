1c1b5d1b8a9fd43eb2aaa73167867686851017c3
CodingFarmer999 <>
2025-11-24 22:15:09 +0800

    Connect to DB

--- exp-login-lab/src/main/java/com/course/service/LoginService.java
+++ exp-login-lab/src/main/java/com/course/service/LoginService.java
@@ -1,12 +1,21 @@
 package com.course.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.course.entity.UserEntity;
+import com.course.repository.UserRepository;
 
 @Service
 public class LoginService {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public boolean checkLogin(String username, String password) {
-		if (username.equals("aaa") && password.equals("1111")) {
+		UserEntity user = userRepository.findByUsername(username);
+		
+		if (user != null && user.getPassword().equals(password)) {
 			return true;
 		} else {
 			return false;
